8ce5956007b789146044934e8307d28ae64013f7
PulkitAsri <pulkit.asri@gmail.com>
2021-06-08 20:50:45 +0530

    added basic help styling

--- src/components/StyledSelector.jsx
+++ src/components/StyledSelector.jsx
@@ -3,9 +3,16 @@ import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import leftClick from "../Images/mouseLeftClick.png";
+import rightClick from "../Images/mouseRightClick.png";
+
 
 
 import { Help } from '@material-ui/icons';
+import { Card } from '@material-ui/core';
 
 const HtmlTooltip = withStyles((theme) => ({
     tooltip: {
@@ -16,6 +23,16 @@ const HtmlTooltip = withStyles((theme) => ({
       border: '1px solid #dadde9',
     },
   }))(Tooltip);
+
+  const InstructionCard = withStyles((theme) => ({
+      card:{
+        padding: 20,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+  }))(Card);
+
+
 
 
 function StyledSelector(props) {
@@ -34,12 +51,34 @@ function StyledSelector(props) {
             <div>
                 <HtmlTooltip
                     title={
-                    <React.Fragment>
-                        <Typography color="inherit">How To Play</Typography>
-
-                        {"Left Click To Reveal a Mine"}
-                        {"Right Click to Flag a cell i.e. mark the cell "}
+                    <React.Fragment >
+                        <Typography  variant="h5" color="inherit">How To Play</Typography>
+                        <div >
+                            <Grid
+                                container
+                                direction="row"
+                                justify="flex-start"
+                                alignItems="center"
+                                style={{padding:20}}
+                            >
+                            
+                            <InstructionCard  square>
+                            <img  style={{width:100}} src={leftClick}/>
+                            <Typography style={{padding:20}} color="inherit">{"Left Click To Reveal a Mine"}</Typography>
+                            
+                            </InstructionCard>
                         
+                        
+                            <InstructionCard  square >
+                            <img  style={{width:100}} src={rightClick}/>
+                            <Typography style={{padding:20}} color="inherit">{"Right Click to Flag a cell i.e. mark the cell "}</Typography>
+
+                            </InstructionCard>
+                            
+
+                        </Grid>
+                        </div>
+
                     </React.Fragment>
                     }
                 >
